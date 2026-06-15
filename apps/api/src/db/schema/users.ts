8fe88126b1f0270fb1a02da68fdccb3150ee649a
godcl1623 <godcl1623@gmail.com>
2026-06-15 23:57:48 +0900

    feat: 스키마 파일 초안 작성

---
+++ apps/api/src/db/schema/users.ts
@@ -0,0 +1,10 @@
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  nickname: varchar("nickname").notNull(),
+  avatarUrl: varchar("avatar_url"),
+  deletedAt: timestamp("deleted_at"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
