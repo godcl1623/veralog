271eb7bbfa9184070af2fa451193a4bf49a9f27a
godcl1623 <godcl1623@gmail.com>
2026-06-16 01:09:31 +0900

    feat: 프로젝트 스키마 추가

---
+++ apps/api/src/db/schema/projects.ts
@@ -0,0 +1,24 @@
+import {
+  boolean,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+import { users } from "./users";
+
+export const projects = pgTable("projects", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  name: varchar("name").notNull(),
+  description: text("description"),
+  isSystem: boolean("is_system").notNull(),
+  deletedAt: timestamp("deleted_at"),
+  expiresAt: timestamp("expires_at"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
