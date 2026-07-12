da4e770783701b226229caa926fafbc909fb181a
godcl1623 <godcl1623@gmail.com>
2026-07-12 12:12:45 +0900

    feat: 인증 페이지 UI 초안 작성

--- apps/web/src/pages/auth/index.tsx
+++ apps/web/src/pages/auth/index.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/shared/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/shared/components/ui/card";
+
 export function AuthPage() {
-  return <div>Auth</div>;
+  return (
+    <main className={"flex size-full items-center justify-center"}>
+      <Card className={"h-1/2 w-1/3 justify-center gap-10"}>
+        <CardHeader>
+          <CardTitle className={"text-center text-3xl font-bold"}>
+            Veralog
+          </CardTitle>
+        </CardHeader>
+        <CardContent className={"text-center"}>
+          <Button variant={"outline"}>Login With Google</Button>
+        </CardContent>
+      </Card>
+    </main>
+  );
 }
