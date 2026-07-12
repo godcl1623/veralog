9c792b9789aec42f523de2fecedb1e55a81e2e43
godcl1623 <godcl1623@gmail.com>
2026-07-12 11:06:30 +0900

    feat: App.css 삭제, 전역 스타일 파일 위치 수정, 인증 페이지 파일 생성

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,12 @@
-import "./index.css";
+import "@/__globals__/styles/index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
+import { App } from "@/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
